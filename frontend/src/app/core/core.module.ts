4a87189bb776b2b8dbb19e790df6dfc568a66d24
Hector Rosales <hector.rosales@acklenavenue.com>
2019-12-23 13:49:36 -0600

    added core module.

---
+++ frontend/src/app/core/core.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { EntityListComponent } from './components/entity-list/entity-list.component';
+import { EntityDetailsComponent } from './components/entity-details/entity-details.component';
+
+
+
+@NgModule({
+  declarations: [EntityListComponent, EntityDetailsComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class CoreModule { }
